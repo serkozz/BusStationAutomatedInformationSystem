184b4fa93663342f6a272fa05174b3a6ebbfb170
Сергей Козлов <serezha-kozlov.2002@mail.ru>
2022-04-07 18:05:05 +0400

    Established connection to database

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,13 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.connectButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // connectButton
+            // 
+            this.connectButton.Font = new System.Drawing.Font("Segoe UI Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.connectButton.Location = new System.Drawing.Point(222, 192);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(333, 44);
+            this.connectButton.TabIndex = 0;
+            this.connectButton.Text = "ПОДКЛЮЧИТЬСЯ К СЕРВЕРУ";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.connectButton);
             this.Name = "MainForm";
             this.Text = "BusStationAutomatedInformationSystem";
             this.ResumeLayout(false);
@@ -42,6 +55,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button connectButton;
     }
 }
 
